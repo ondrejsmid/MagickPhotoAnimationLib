b9303e7ff3abe1bbdd4b710fb8700e5435560b4e
ondsmid <ondsmid@seznam.cz>
2022-08-10 14:01:43 +0200

    crop on center

--- MagickPhotoAnimationLib/MainWindow.xaml.cs
+++ MagickPhotoAnimationLib/MainWindow.xaml.cs
@@ -22,35 +22,52 @@ namespace MagickPhotoAnimationLib
     public partial class MainWindow : Window
     {
         private const int WindowSizeCategory = 1;
-        private readonly int[] WindowWidthCategories = new int[] { 200, 400 };
-
-        private const float VideoRatio = 1.5f;
+        private readonly int[] WindowWidthCategories = new int[] { 10, 200, 400 };
+                                                                // 0   1    2
+        private const float OutputScreenRatio = 1.5f;
+        private const int OutputFrameRate = 1;
 
         public MainWindow()
         {
             InitializeComponent();
 
             Image1.Width = WindowWidthCategories[WindowSizeCategory];
-            Image1.Height = Image1.Width / VideoRatio;
+            Image1.Height = Image1.Width / OutputScreenRatio;
             Top = SystemParameters.PrimaryScreenHeight - Image1.Height - 85;
 
             var img = new MagickImage(@"C:\Users\ondrej\MagickPhotoAnimationLib\images\1.jpg");
 
-            var croppedWidth = 2000;
-            img.Crop(new MagickGeometry(2000, 1000, croppedWidth, (int)(croppedWidth / VideoRatio)));
+
+
+            const float animTimeInSec = 3.572f;
+
+            var startCropWidth = img.Width;
+            var startCropHeight = img.Height;
+            var startCropX = 0;
+            var startCropY = 0;
+
+            var endCropWidth = 2000;
+            var endCropHeight = (int)(endCropWidth / OutputScreenRatio);
+            var endCropX = (startCropWidth - endCropWidth) / 2;
+            var endCropY = (startCropHeight - endCropHeight) / 2;
+
+            img.Write(@"C:\Users\ondrej\MagickPhotoAnimationLib\out\sequence\start.jpg");
+            
+            var img2 = img.Clone();
+            img.Dispose();
+            
+            img2.Crop(new MagickGeometry(endCropX, endCropY, endCropWidth, endCropHeight));
+            img2.Write(@"C:\Users\ondrej\MagickPhotoAnimationLib\out\sequence\end.jpg");
             
             
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            img.Write(@"C:\Users\ondrej\MagickPhotoAnimationLib\images\2.jpg");
-            Image1.Source = new BitmapImage(new Uri(@"C:\Users\ondrej\MagickPhotoAnimationLib\images\2.jpg"));
+
+
+
+            //img.Crop(new MagickGeometry(2000, 1000, croppedWidth, (int)(croppedWidth / OutputScreenRatio)));
+            //img.Crop(croppedWidth, (int)(croppedWidth / OutputScreenRatio));
+
+
+
         }
     }
 }
