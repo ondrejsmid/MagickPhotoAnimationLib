0689dd339100bf121336ae3c5e7a7b89aeee7a0b
ondsmid <ondsmid@seznam.cz>
2022-08-10 16:33:27 +0200

    ObfuscationMinimization

--- MagickPhotoAnimationLib/MainWindow.xaml.cs
+++ MagickPhotoAnimationLib/MainWindow.xaml.cs
@@ -21,73 +21,66 @@ namespace MagickPhotoAnimationLib
     /// </summary>
     public partial class MainWindow : Window
     {
-        private const int WindowSizeCategory = 1;
-        private readonly int[] WindowWidthCategories = new int[] { 10, 200, 400 };
-                                                                // 0   1    2
+        private static float[] ObfuscationMinimizations = new float[] { 1, 0.1f };
+        private static float ObfuscationMinimization = ObfuscationMinimizations[1];
+
         private const float OutputScreenRatio = 1.5f;
         private const int OutputFrameRate = 1;
+
+        private int _outputIndex = 0;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            Image1.Width = WindowWidthCategories[WindowSizeCategory];
-            Image1.Height = Image1.Width / OutputScreenRatio;
             Top = SystemParameters.PrimaryScreenHeight - Image1.Height - 85;
 
-            var img = new MagickImage(@"C:\Users\ondrej\MagickPhotoAnimationLib\images\1.jpg");
-
-
+            var startImg = new MagickImage(@"C:\Users\ondrej\MagickPhotoAnimationLib\images\1.jpg");
 
             const float animTimeInSec = 3.572f;
 
-            var startCropWidth = img.Width;
-            var startCropHeight = img.Height;
-            var startCropX = 0;
-            var startCropY = 0;
+            var startCropWidth = startImg.Width;
+            var startCropHeight = startImg.Height;
 
-            var endCropWidth = 2000;
+            var endCropWidth = 3000;
             var endCropHeight = (int)(endCropWidth / OutputScreenRatio);
-            var endCropX = (startCropWidth - endCropWidth) / 2;
-            var endCropY = (startCropHeight - endCropHeight) / 2;
 
-            img.Write(@"C:\Users\ondrej\MagickPhotoAnimationLib\out\start_end\start.jpg");
-            
-            var img2 = img.Clone();
-            
-            img2.Crop(new MagickGeometry(endCropX, endCropY, endCropWidth, endCropHeight));
-            img2.Write(@"C:\Users\ondrej\MagickPhotoAnimationLib\out\start_end\end.jpg");
+            startImg.Write(@"C:\Users\ondrej\MagickPhotoAnimationLib\out\start_end\1_start.jpg");
+
+            var endImg = startImg.Clone();
+
+            endImg.Crop(endCropWidth, endCropHeight, Gravity.Center);
+            endImg.Write(@"C:\Users\ondrej\MagickPhotoAnimationLib\out\start_end\2_end.jpg");
 
             var animFrameCount = Math.Ceiling(animTimeInSec * OutputFrameRate);
 
             IMagickImage currentImg;
 
-            for (int i = 0; i < animFrameCount; i ++)
+            for (int i = 0; i < animFrameCount; i++)
             {
-                currentImg = img.Clone();
+                currentImg = startImg.Clone();
 
                 const float OutputFrameTimeInSec = 1 / OutputFrameRate;
                 var animTimePositionInSec = (i + 0.5) * OutputFrameTimeInSec;
                 var animTimePositionRatio = animTimePositionInSec / animTimeInSec;
 
-                var currentCropX = startCropX + (int)((endCropX - startCropX) * animTimePositionRatio);
-                var currentCropY = startCropY + (int)((endCropY - startCropY) * animTimePositionRatio);
                 var currentCropWidth = startCropWidth + (int)((endCropWidth - startCropWidth) * animTimePositionRatio);
                 var currentCropHeight = startCropHeight + (int)((endCropHeight - startCropHeight) * animTimePositionRatio);
 
-                currentImg.Crop(new MagickGeometry(currentCropX, currentCropY, currentCropWidth, currentCropHeight));
-               
-                currentImg.Write($@"C:\Users\ondrej\MagickPhotoAnimationLib\out\sequence\{i.ToString("00")}.jpg");
-                currentImg.Dispose();
+                currentImg.Crop(currentCropWidth, currentCropHeight, Gravity.Center);
+
+                WriteAndDispose(currentImg);
             }
 
+            startImg.Dispose();
+        }
+
+        private void WriteAndDispose(IMagickImage img)
+        {
+            img.Resize(new Percentage(ObfuscationMinimization * 100));
+            img.Write($@"C:\Users\ondrej\MagickPhotoAnimationLib\out\sequence\{_outputIndex.ToString("00")}.jpg");
             img.Dispose();
-
-            //img.Crop(new MagickGeometry(2000, 1000, croppedWidth, (int)(croppedWidth / OutputScreenRatio)));
-            //img.Crop(croppedWidth, (int)(croppedWidth / OutputScreenRatio));
-
-
-
+            _outputIndex++;
         }
     }
 }
